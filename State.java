e9ca82659c9ddaacd430717d2ac05cb0b20f0cb3
ThanasisTh <thanasisdtl@gmail.com>
2017-11-02 23:04:21 +0200

    More heuristic checks in State.java
    Added sparse periods check, changed children generation process, turns out it was fine.
    Also added slotsFilled as a hashCode variable and prepared class constructors.

--- State.java
+++ State.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
 import java.util.ArrayList;
@@ -17,22 +16,35 @@ public class State implements Comparable<State>{
 								15, 25 ,35, 45, 55,
 								16, 26, 36, 46, 56,
 								17, 27, 37, 47, 57 }; //11 == Monday - 1st hour, 21 == Tuesday - 1st hour, etc ;
+	private int slotsFilled;
+	private Vector<String> classNames;
 	
-	public State() {
-		this.schedule = null;
-		this.score = 0;
+	public State(HashMap<String, Vector<Lesson>> classLessons, HashMap<Integer, Teacher> teachers) {
+		this.schedule = new Slot[teachers.size()][this.dayHours.length];
+		this.score = -1;
+		this.slotsFilled = 0;
+		for(String s : classLessons.keySet()) {
+			classNames.add(s);
+		}
+		this.classLessons = classLessons;
+		this.teachers = teachers;
+		
 	}
 	
-	public State(Slot[][] schedule) {
+	public State(Slot[][] schedule, HashMap<String, Vector<Lesson>> classLessons, HashMap<Integer, Teacher> teachers, int slotsFilled, Vector<String> classNames) {
 		this.schedule = schedule;
-		//TODO: read data from files
+		this.score = -1;
+		this.slotsFilled = slotsFilled;
+		this.classNames = classNames;
+		this.classLessons = classLessons;
+		this.teachers = teachers;
 	}
 	
 	public boolean assignSlot(String cKey, String lKey, int tKey, int dayHour) {
-		if(this.schedule[dayHour][tKey] != null) {
+		if(this.schedule[tKey][dayHour] != null) {
 			return false;
 		}
-		this.schedule[dayHour][tKey].setSlot(cKey, lKey);
+		this.schedule[tKey][dayHour].setSlot(cKey, lKey);
 		return true;
 	}
 	
@@ -42,39 +54,39 @@ public class State implements Comparable<State>{
      */
 	public ArrayList<State> getChildren(int heuristic) {
 		ArrayList<State> children = new ArrayList<State>();
-		State child = new State(this.schedule);
+		State child = new State(this.schedule, this.classLessons, this.teachers, this.slotsFilled, this.classNames);
 		
-		Iterator<Lesson> iterator;
-		
-		//TODO check generating process
-		for(Map.Entry<Integer, Teacher> t : teachers.entrySet()) {
-			for(Map.Entry<String, Vector<Lesson>> c : classLessons.entrySet()) {
-				iterator = c.getValue().iterator();
-				for(int d=0; d<dayHours.length; d++) {
-					
-					//should be ok now	
-					Lesson next = iterator.next();
-					if(t.getValue().getLKeys().contains(next.getKey())) {
-						if(teachers.get(t.getKey()).getWeekHours()!=0 && next.getHours()!=0) {
-							if(teachers.get(t.getKey()).getDayHours()[dayHours[d]/10-1]!=0) {
-								if(child.assignSlot(c.getKey(), next.getKey(), t.getKey(), d)) {
-									child.evaluate(); //use heuristic functions
-									children.add(child);
-									child = new State(this.schedule);
-									next.decreaseHours();
-									teachers.get(t.getKey()).decreaseDayHours(dayHours[d]/10-1);
+		//generating process should be 100% complete
+		for(Map.Entry<String, Vector<Lesson>> c : this.classLessons.entrySet()) {
+			for(int d=0; d<dayHours.length; d++) {
+				for(Lesson l : c.getValue()) {
+					for(Map.Entry<Integer, Teacher> t : teachers.entrySet()) {
+						
+						//should be ok now	
+						if(t.getValue().getLKeys().contains(l.getKey())) {
+							if(teachers.get(t.getKey()).getWeekHours()!=0 && l.getHours()!=0) {
+								if(teachers.get(t.getKey()).getDayHours()[dayHours[d]/10-1]!=0) {
+									if(child.assignSlot(c.getKey(), l.getKey(), t.getKey(), d)) {
+										child.evaluate();
+										l.decreaseHours();
+										teachers.get(t.getKey()).decreaseDayHours(dayHours[d]/10-1);
+										this.slotsFilled++;
+										children.add(child);
+										child = new State(this.schedule, this.classLessons, this.teachers, this.slotsFilled, this.classNames);
+									}
 								}
 							}
-						}
-						if(next.getHours()==0) {
-							c.getValue().remove(next);
-							if(c.getValue().isEmpty()) {
-								classLessons.remove(c.getKey());
+							if(l.getHours()==0) {
+								c.getValue().remove(l);
+								if(c.getValue().isEmpty()) {
+									this.classLessons.remove(c.getKey());
+								}
+							}
+							if(teachers.get(t.getKey()).getWeekHours()==0) {
+								teachers.remove(t.getKey());
 							}
 						}
-						if(teachers.get(t.getKey()).getWeekHours()==0) {
-							teachers.remove(t.getKey());
-						}
+						
 					}
 				}
 			}
@@ -82,19 +94,29 @@ public class State implements Comparable<State>{
 		return children;
 	}
 	
-	//Checks if a teacher gets more than two consecutive hours, or if any teachers get more hours than others
+	//Checks if a teacher gets more than two consecutive hours, if any teachers get more hours than others, or if any classes have empty periods
 	public void heuristicA() {
+		HashMap<Integer, Vector<String>> classHours = new HashMap<Integer, Vector<String>>();
+		classHours.put(0, null);
+		classHours.put(1, null);
+		classHours.put(2, null);
+		classHours.put(3, null);
+		classHours.put(4, null);
+		classHours.put(5, null);
+		classHours.put(6, null);
+		
 		int intensiveCounter = 0;
-		int[] fairnessCounters = new int[this.schedule[1].length];
-		for(int i=0; i<this.schedule[1].length; i++) {
+		int[] fairnessCounters = new int[this.schedule.length];
+		for(int i=0; i<this.schedule.length; i++) {
 			for(int j=0; j<4; j++) {
 				for(int k=0; k<6; k++) {	
-					if(this.schedule[j*10+k][i]!=null) {
+					if(this.schedule[i][j*10+k]!=null) {
 						intensiveCounter++;
 						fairnessCounters[i]++;
 						if(intensiveCounter>2) {
 							this.score++;
 						}
+						classHours.get(k).add(this.schedule[i][j*10+k].getCKey());
 					}
 					else {
 						intensiveCounter = 0;
@@ -114,29 +136,29 @@ public class State implements Comparable<State>{
 				this.score++;
 			}
 		}
+		for(int i=0; i<classHours.size(); i++) {
+			for(String s : classNames) {
+				if(!classHours.get(i).contains(s)) {
+					this.score++;
+				}
+			}
+		}
 	}
 	
 	//TODO checks if the classes' lessons are distributed uniformly in the week
 	public boolean isUniform() {
+		
 		return true;
 	}
-	
-	//TODO checks if empty periods exist for classes
-	public boolean isSparse() {
-		return true;
-	}
-	
+		
 	private void evaluate() {
 		this.heuristicA();
-		if(this.isSparse()) {
-			this.score++;
-		}
 		if(!this.isUniform()) {
 			this.score++;
 		}
 	}
 	
-	//TODO state is terminal if all classes and lessons have been assigned
+	//state is terminal if all classes and lessons have been assigned
 	public boolean isTerminal() {
 		if(this.classLessons.isEmpty() || this.teachers.isEmpty()) {
 			return true;
@@ -144,9 +166,15 @@ public class State implements Comparable<State>{
 		return false;
 	}
 	
+	//TODO
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
+	
 	@Override
 	public int hashCode() {
-		return 1;
+		return this.slotsFilled;
 	}
 	
 	@Override
